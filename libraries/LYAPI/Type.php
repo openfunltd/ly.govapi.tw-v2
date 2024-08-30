68134be17a4e8c603f443531de2485acf777b5ac
Ronny Wang <ronnywang@gmail.com>
2024-08-30 16:38:11 +0800

    add LYAPI_Type::run($type, $method) to call LYAPI_Type_{$type}::method() and add default getReturnKey()

--- libraries/LYAPI/Type.php
+++ libraries/LYAPI/Type.php
@@ -2,6 +2,20 @@
 
 class LYAPI_Type
 {
+    public static function run($type, $method)
+    {
+        $class = 'LYAPI_Type_' . ucfirst($type);
+        if (!class_exists($class)) {
+            throw new Exception('Type not found: ' . $type);
+        }
+        return call_user_func([$class, $method]);
+    }
+
+    public static function getReturnKey()
+    {
+        return strtolower(str_replace('LYAPI_Type_', '', get_called_class())) . 's';
+    }
+
     public static function get_id_count()
     {
         return 1;
