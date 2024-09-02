e2f527ff5d6a7f02f8c992cb5fa8600cd10e89b0
Ronny Wang <ronnywang@gmail.com>
2024-09-02 14:19:07 +0800

    add Meet more field map

--- libraries/LYAPI/Type/Meet.php
+++ libraries/LYAPI/Type/Meet.php
@@ -13,11 +13,21 @@ class LYAPI_Type_Meet extends LYAPI_Type
             'sessionTimes' => '會次',
             'dates' => '日期',
             'title' => '會議標題',
-            'meet_data' => '會議資料',
+            'meet_data' => '會議資料', // from https://data.ly.gov.tw/getds.action?id=42
             "meet_data.term" => '屆期',
             'meet_data.sessionPeriod' => '會期',
-            'meet_data.attendLegislator' => '出席立委',
+            'meet_data.sessionTimes' => '會次',
+            'meet_data.meetingTimes' => '臨時會會次',
             'meet_data.meetingNo' => '會議編號',
+            'meet_data.meetingDateDesc' => '會議時間區間',
+            'meet_data.meetingRoom' => '會議地點',
+            'meet_data.meetingUnit' => '會議單位',
+            'meet_data.jointCommittee' => '聯席委員會',
+            'meet_data.meetingName' => '會議名稱',
+            'meet_data.meetingContent' => '會議事由',
+            'meet_data.coChairman' => '委員會召集委員',
+            'meet_data.attendLegislator' => '出席委員',
+            'meet_data.selectTerm' => '屆別期別篩選條件',
         ];
     }
 
