165740857fd2be1922791fc4f7d297f084ee1ec3
Ronny Wang <ronnywang@gmail.com>
2024-09-04 11:52:49 +0800

    update bill api config

--- libraries/LYAPI/Type/Bill.php
+++ libraries/LYAPI/Type/Bill.php
@@ -10,16 +10,38 @@ class LYAPI_Type_Bill extends LYAPI_Type
             'mtime' => '資料抓取時間',
             'first_time' => '提案日期',
             'last_time' => '最新進度日期',
+            'laws' => '法律編號',
+            '屆期' => '屆',
         ];
     }
 
     public static function filterFields()
     {
         return [
-            '屆期' => '',
             '議案流程.狀態' => '議案流程.狀態.keyword',
             '提案人' => '提案人.keyword',
             '連署人' => '連署人.keyword',
+            '法律編號' => 'laws.keyword',
+            '議案狀態' => '議案狀態.keyword',
+            '會議代碼' => 'meet_id.keyword',
+            '議案類別' => '議案類別.keyword',
+            '提案來源' => '提案來源.keyword',
+            '議案編號' => '',
+            '屆' => '',
+            '會期' => '',
+            '提案編號' => '提案編號.keyword',
+        ];
+    }
+
+    public static function queryFields()
+    {
+        return [
+            '議案名稱',
+            '提案單位/提案委員',
+            '提案人',
+            '連署人',
+            '案由',
+            '說明',
         ];
     }
 
@@ -39,6 +61,13 @@ class LYAPI_Type_Bill extends LYAPI_Type
             '會期',
             '字號',
             '提案編號',
+        ];
+    }
+
+    public static function sortFields()
+    {
+        return [
+            '資料抓取時間',
         ];
     }
 }
