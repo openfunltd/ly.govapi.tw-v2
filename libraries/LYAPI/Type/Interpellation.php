b79a9b29668dff8ad3536f71e6d986de3b239533
Ronny Wang <ronnywang@gmail.com>
2024-09-04 14:30:53 +0800

    update interpellation api config

--- libraries/LYAPI/Type/Interpellation.php
+++ libraries/LYAPI/Type/Interpellation.php
@@ -7,6 +7,17 @@ class LYAPI_Type_Interpellation extends LYAPI_Type
         return [
             'term' => '屆期',
             'legislators' => '質詢委員',
+            'meet_id' => '會議代碼',
+            'id' => '質詢編號',
+            'page_start' => '質詢起始頁',
+            'page_end' => '質詢結束頁',
+            'printed_at' => '刊登日期',
+            'reason' => '事由',
+            'description' => '說明',
+            'meetingNo' => '會議編號',
+            'sessionPeriod' => '會期',
+            'sessionTimes' => '會次',
+            'meetingDate' => '會議日期',
         ];
     }
 
@@ -15,6 +26,7 @@ class LYAPI_Type_Interpellation extends LYAPI_Type
         return [
             '質詢委員' => 'legislators.keyword',
             '屆期' => '',
+            '會議代碼' => 'meet_id.keyword',
         ];
     }
 }
