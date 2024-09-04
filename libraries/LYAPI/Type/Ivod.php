e2edf0c8b82afe7cbafe762f2183c36830f72949
Ronny Wang <ronnywang@gmail.com>
2024-09-04 14:38:13 +0800

    update ivod config

--- libraries/LYAPI/Type/Ivod.php
+++ libraries/LYAPI/Type/Ivod.php
@@ -17,14 +17,30 @@ class LYAPI_Type_Ivod extends LYAPI_Type
             'meet.type' => '種類',
             'meet.committees' => '委員會代碼',
             'meet.title' => '標題',
-
+            'type' => '影片種類',
+            'start_time' => '開始時間',
+            'end_time' => '結束時間',
+            'duration' => '影片長度',
+            'features' => '支援功能',
         ];
     }
 
     public static function filterFields()
     {
         return [
+            '屆' => 'meet.term',
+            '會期' => 'meet.sessionPeriod',
             '會議.會議代碼' => 'meet.id.keyword',
+            '委員名稱' => '委員名稱.keyword',
+            '會議資料.委員會代碼' => '',
+            '日期' => '',
+        ];
+    }
+
+    public static function sortFields()
+    {
+        return [
+            '會議時間',
         ];
     }
 }
