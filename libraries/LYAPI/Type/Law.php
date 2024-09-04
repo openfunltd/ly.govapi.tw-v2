02349a2081398c9c2b5e7996e90babf062ca17a2
Ronny Wang <ronnywang@gmail.com>
2024-09-04 17:22:12 +0800

    add law api

---
+++ libraries/LYAPI/Type/Law.php
@@ -0,0 +1,46 @@
+<?php
+
+class LYAPI_Type_Law extends LYAPI_Type
+{
+    public static function getFieldMap()
+    {
+        // https://data.ly.gov.tw/
+        //  => https://data.ly.gov.tw/odw/LawNo.pdf 法律編號
+        return [
+            'id' => '法律編號',
+            'type' => '類別',
+            'parent' => '母法編號',
+            'name' => '名稱',
+            'name_other' => '其他名稱',
+        ];
+    }
+
+    public static function filterFields()
+    {
+        return [
+            '法律編號' => '',
+            '類別' => 'type.keyword',
+            '母法編號' => 'parent.keyword',
+        ];
+    }
+
+    public static function queryFields()
+    {
+        return [
+            '名稱',
+            '其他名稱',
+        ];
+    }
+
+    public static function getRelations()
+    {
+        return [
+            'bills' => [
+                'type' => 'bill',
+                'map' => [
+                    '法律編號' => '法律編號',
+                ],
+            ],
+        ];
+    }
+}
