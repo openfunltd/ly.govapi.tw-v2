766d7952f4583ee5a594716c981ca9585af2bc29
Ronny Wang <ronnywang@gmail.com>
2024-09-04 13:40:51 +0800

    add ivod api

---
+++ libraries/LYAPI/Type/Ivod.php
@@ -0,0 +1,16 @@
+<?php
+
+class LYAPI_Type_Ivod extends LYAPI_Type
+{
+    public static function getFieldMap()
+    {
+        return [
+        ];
+    }
+
+    public static function filterFields()
+    {
+        return [
+        ];
+    }
+}
