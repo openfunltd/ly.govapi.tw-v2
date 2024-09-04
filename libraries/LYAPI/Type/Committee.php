e0d73938338e26343d55aa30df2e29357418453f
Ronny Wang <ronnywang@gmail.com>
2024-09-04 09:40:06 +0800

    add Committee api

---
+++ libraries/LYAPI/Type/Committee.php
@@ -0,0 +1,35 @@
+<?php
+
+class LYAPI_Type_Committee extends LYAPI_Type
+{
+    public static function getFieldMap()
+    {
+        // https://data.ly.gov.tw/getds.action?id=14
+        // comtcd：委員會代號, comtName：委員會名稱, comtDesp：委員會/職掌, comtType：委員會類別(1:常設委員會2:特種委員會3:國會改革前舊委員會名稱)
+        return [
+            'comtCd' => '委員會代號',
+            'comtName' => '委員會名稱',
+            'comtDesp' => '委員會職掌',
+            'comtType' => '委員會類別',
+        ];
+    }
+
+    public static function filterFields()
+    {
+        return [
+            '委員會類別' => '',
+        ];
+    }
+
+    public static function getRelations()
+    {
+        return [
+            'meet' => [
+                'type' => 'meet',
+                'map' => [
+                    '委員會代號' => '委員會代號',
+                ],
+            ],
+        ];
+    }
+}
