a7e965e5b1e4971b0d5c9e5339c5a12fb587c8ea
Ronny Wang <ronnywang@gmail.com>
2024-09-04 14:02:52 +0800

    update meet api config

--- libraries/LYAPI/Type/Meet.php
+++ libraries/LYAPI/Type/Meet.php
@@ -5,7 +5,7 @@ class LYAPI_Type_Meet extends LYAPI_Type
     public static function getFieldMap()
     {
         return [
-            'term' => '屆期',
+            'term' => '屆',
             'meet_id' => '會議代碼',
             'meet_type' => '會議種類',
             'committees' => '委員會代號',
@@ -14,7 +14,7 @@ class LYAPI_Type_Meet extends LYAPI_Type
             'dates' => '日期',
             'title' => '會議標題',
             'meet_data' => '會議資料', // from https://data.ly.gov.tw/getds.action?id=42
-            "meet_data.term" => '屆期',
+            "meet_data.term" => '屆',
             'meet_data.sessionPeriod' => '會期',
             'meet_data.sessionTimes' => '會次',
             'meet_data.meetingTimes' => '臨時會會次',
@@ -28,13 +28,16 @@ class LYAPI_Type_Meet extends LYAPI_Type
             'meet_data.coChairman' => '委員會召集委員',
             'meet_data.attendLegislator' => '出席委員',
             'meet_data.selectTerm' => '屆別期別篩選條件',
+            'meet_data.date' => '日期',
+            'meet_data.startTime' => '開始時間',
+            'meet_data.endTime' => '結束時間',
         ];
     }
 
     public static function filterFields()
     {
         return [
-            '屆期' => '',
+            '屆' => '',
             '會期' => '',
             '會議種類' => 'meet_type.keyword',
             '會議資料.出席立委' => 'meet_data.attendLegislator.keyword',
@@ -53,4 +56,24 @@ class LYAPI_Type_Meet extends LYAPI_Type
         ];
     }
 
+    public static function sortFields()
+    {
+        return [
+            '屆',
+            '會期',
+            '日期',
+        ];
+    }
+
+    public static function getRelations()
+    {
+        return [
+            'ivod' => [
+                'type' => 'ivod',
+                'map' => [
+                    '會議代碼' => '會議.會議代碼',
+                ],
+            ]
+        ];
+    }
 }
