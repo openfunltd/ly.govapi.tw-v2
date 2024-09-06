a765ffc71c50b9fcecac5fdaeef77f26e1d215a8
Ronny Wang <ronnywang@gmail.com>
2024-09-06 13:51:14 +0800

    change api to cors_json

--- controllers/ApiController.php
+++ controllers/ApiController.php
@@ -4,11 +4,11 @@ class ApiController extends MiniEngine_Controller
 {
     public function collectionsAction($type)
     {
-        return $this->json(LYAPI_SearchAction::getCollections($type, $_SERVER['QUERY_STRING']));
+        return $this->cors_json(LYAPI_SearchAction::getCollections($type, $_SERVER['QUERY_STRING']));
     }
 
     public function itemAction($type, $id, $sub)
     {
-        return $this->json(LYAPI_SearchAction::getItem($type, $id, $sub, $_SERVER['QUERY_STRING']));
+        return $this->cors_json(LYAPI_SearchAction::getItem($type, $id, $sub, $_SERVER['QUERY_STRING']));
     }
 }
