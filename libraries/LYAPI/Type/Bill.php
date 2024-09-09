cd9057f488b6875ea4876150549998d2bea93d75
Ronny Wang <ronnywang@gmail.com>
2024-09-09 13:36:12 +0800

    update bill aggs

--- libraries/LYAPI/Type/Bill.php
+++ libraries/LYAPI/Type/Bill.php
@@ -31,7 +31,7 @@ class LYAPI_Type_Bill extends LYAPI_Type
             '會議代碼' => 'meet_id.keyword',
             '議案類別' => '議案類別.keyword',
             '提案來源' => '提案來源.keyword',
-            '議案編號' => '',
+            '議案編號' => 'BillNo.keyword',
             '屆' => '',
             '會期' => '',
             '提案編號' => '提案編號.keyword',
