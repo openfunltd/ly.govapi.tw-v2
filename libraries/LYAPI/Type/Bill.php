6fee54419433ed3a38dde683690b44d8edff77a6
Ronny Wang <ronnywang@gmail.com>
2024-09-11 16:56:39 +0800

    update filter order

--- libraries/LYAPI/Type/Bill.php
+++ libraries/LYAPI/Type/Bill.php
@@ -23,17 +23,17 @@ class LYAPI_Type_Bill extends LYAPI_Type
     public static function filterFields()
     {
         return [
+            '屆' => '',
+            '會期' => '',
             '議案流程.狀態' => '議案流程.狀態.keyword',
+            '議案類別' => '議案類別.keyword',
             '提案人' => '提案人.keyword',
             '連署人' => '連署人.keyword',
             '法律編號' => 'laws.keyword',
             '議案狀態' => '議案狀態.keyword',
             '會議代碼' => 'meet_id.keyword',
-            '議案類別' => '議案類別.keyword',
             '提案來源' => '提案來源.keyword',
             '議案編號' => 'BillNo.keyword',
-            '屆' => '',
-            '會期' => '',
             '提案編號' => '提案編號.keyword',
         ];
     }
