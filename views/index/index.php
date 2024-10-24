7cabacbc4c5368f580f8229a4d4147dfeb1426dd
yo <luyotw@openfun.tw>
2024-10-24 20:07:46 +0800

    add swagger for index

--- views/index/index.php
+++ views/index/index.php
@@ -1,3 +1,19 @@
-<?= $this->partial('common/header') ?>
-This is Index page
-<?= $this->partial('common/footer') ?>
+<!-- HTML for static distribution bundle build -->
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <title>Swagger UI</title>
+    <link rel="stylesheet" type="text/css" href="/public/swagger-ui/swagger-ui.css" />
+    <link rel="stylesheet" type="text/css" href="/public/swagger-ui/index.css" />
+    <link rel="icon" type="image/png" href="/public/swagger-ui/favicon-32x32.png" sizes="32x32" />
+    <link rel="icon" type="image/png" href="/public/swagger-ui/favicon-16x16.png" sizes="16x16" />
+  </head>
+
+  <body>
+    <div id="swagger-ui"></div>
+    <script src="/public/swagger-ui/swagger-ui-bundle.js" charset="UTF-8"> </script>
+    <script src="/public/swagger-ui/swagger-ui-standalone-preset.js" charset="UTF-8"> </script>
+    <script src="/public/swagger-ui/swagger-initializer.js" charset="UTF-8"> </script>
+  </body>
+</html>
