e69208a51161204af278cb76989c7c13ab7cbac7
Ronny Wang <ronnywang@gmail.com>
2025-03-25 10:30:03 +0800

    add production config

--- init.inc.php
+++ init.inc.php
@@ -5,6 +5,8 @@ define('MINI_ENGINE_ROOT', __DIR__);
 include(__DIR__ . '/mini-engine.php');
 if (file_exists(__DIR__ . '/config.inc.php')) {
     include(__DIR__ . '/config.inc.php');
+} elseif (file_exists("/srv/config/v2.ly.govapi.tw.inc.php")) {
+    include("/srv/config/v2.ly.govapi.tw.inc.php");
 }
 set_include_path(
     __DIR__ . '/libraries'
