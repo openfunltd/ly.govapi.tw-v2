442a67da5e7de794363019e7fa366820fda2dd65
Ronny Wang <ronnywang@gmail.com>
2025-03-25 11:17:55 +0800

    處理 /v2 開頭網址

--- index.php
+++ index.php
@@ -1,6 +1,10 @@
 <?php
 include(__DIR__ . '/init.inc.php');
 
+if (strpos($_SERVER['REQUEST_URI'], '/v2') === 0) {
+    $_SERVER['REQUEST_URI'] = substr($_SERVER['REQUEST_URI'], 3);
+}
+
 MiniEngine::dispatch(function($uri){
     if ($uri == '/robots.txt') {
         return ['index', 'robots'];
