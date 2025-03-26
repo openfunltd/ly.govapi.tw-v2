cff82761a004a2302eb27648402c9a4a9fac03a0
typingmonk <tmonk.hsieh@tmonk.tw>
2025-03-26 09:30:43 +0800

    Make swagger-ui using v2/swagger.yaml and using cdnjs

--- views/swagger/ui.php
+++ views/swagger/ui.php
@@ -4,7 +4,7 @@
   <head>
     <meta charset="UTF-8">
     <title>Swagger UI</title>
-    <link rel="stylesheet" type="text/css" href="public/swagger-ui/swagger-ui.css" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/5.17.14/swagger-ui.css" integrity="sha512-MvYROlKG3cDBPskMQgPmkNgZh85LIf68y7SZ34TIppaIHQz1M/3S/yYqzIfufdKDJjzB9Qu1BV63SZjimJkPvw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <link rel="stylesheet" type="text/css" href="public/swagger-ui/index.css" />
     <link rel="icon" type="image/png" href="public/swagger-ui/favicon-32x32.png" sizes="32x32" />
     <link rel="icon" type="image/png" href="public/swagger-ui/favicon-16x16.png" sizes="16x16" />
@@ -12,8 +12,8 @@
 
   <body>
     <div id="swagger-ui"></div>
-    <script src="public/swagger-ui/swagger-ui-bundle.js" charset="UTF-8"> </script>
-    <script src="public/swagger-ui/swagger-ui-standalone-preset.js" charset="UTF-8"> </script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/5.17.14/swagger-ui-bundle.js" integrity="sha512-mVvFSCxt0sK0FeL8C7n8BcHh10quzdwfxQbjRaw9pRdKNNep3YQusJS5e2/q4GYt4Ma5yWXSJraoQzXPgZd2EQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/5.17.14/swagger-ui-standalone-preset.js" integrity="sha512-DgicCd4AI/d7/OdgaHqES3hA+xJ289Kb5NmMEegbN8w/Dxn5mvvqr9szOR6TQC+wjTTMeqPscKE4vj6bmAQn6g==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
     <script src="public/swagger-ui/swagger-initializer.js" charset="UTF-8"> </script>
   </body>
 </html>
