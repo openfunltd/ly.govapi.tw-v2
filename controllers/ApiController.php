1b39f5311ec2b85bf286b84dd504c18217a7a1a6
Ronny Wang <ronnywang@gmail.com>
2025-05-15 14:32:53 +0800

    add api logger

--- controllers/ApiController.php
+++ controllers/ApiController.php
@@ -4,11 +4,27 @@ class ApiController extends MiniEngine_Controller
 {
     public function collectionsAction($type)
     {
-        return $this->cors_json(LYAPI_SearchAction::getCollections($type, $_SERVER['QUERY_STRING']));
+        OpenFunAPIHelper::checkUsage([
+            'project' => 'lyapi2',
+            'class' => "{$type}_collection",
+        ]);
+        $ret = LYAPI_SearchAction::getCollections($type, $_SERVER['QUERY_STRING']);
+        OpenFunAPIHelper::apiDone([
+            'size' => strlen(json_encode($ret, JSON_UNESCAPED_UNICODE)),
+        ]);
+        return $this->cors_json($ret);
     }
 
     public function itemAction($type, $id, $sub)
     {
-        return $this->cors_json(LYAPI_SearchAction::getItem($type, $id, $sub, $_SERVER['QUERY_STRING']));
+        OpenFunAPIHelper::checkUsage([
+            'project' => 'lyapi2',
+            'class' => "{$type}_item",
+        ]);
+        $ret = LYAPI_SearchAction::getItem($type, $id, $sub, $_SERVER['QUERY_STRING']);
+        OpenFunAPIHelper::apiDone([
+            'size' => strlen(json_encode($ret, JSON_UNESCAPED_UNICODE)),
+        ]);
+        return $this->cors_json($ret);
     }
 }
