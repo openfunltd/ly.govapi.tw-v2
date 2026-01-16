c322ff838179020e20e33b570547bc0e9a36659c
Ronny Wang <ronnywang@gmail.com>
2026-01-16 13:46:44 +0800

    遇到錯誤提供錯誤訊息，不要噴 500

--- controllers/ApiController.php
+++ controllers/ApiController.php
@@ -8,7 +8,23 @@ class ApiController extends MiniEngine_Controller
             'project' => 'lyapi2',
             'class' => "{$type}_collection",
         ]);
-        $ret = LYAPI_SearchAction::getCollections($type, $_SERVER['QUERY_STRING']);
+        try {
+            $ret = LYAPI_SearchAction::getCollections($type, $_SERVER['QUERY_STRING']);
+        } catch (Exception $e) {
+            $uniqid = MiniEngineHelper::uniqid(8);
+            if (strpos($e->getMessage(), 'Result window is too large')) {
+                header('HTTP/1.1 413 Payload Too Large', true, 413);
+                $message = "錯誤，請縮小查詢範圍或調整分頁參數後重試";
+            } else {
+                header('HTTP/1.1 500 Internal Server Error', true, 500);
+                $message = "錯誤，錯誤代碼為 " . $uniqid;
+                error_log("[$uniqid] " . $e->getMessage());
+            }
+            $ret = [
+                'error' => true,
+                'message' => $message,
+            ];
+        }
         OpenFunAPIHelper::apiDone([
             'size' => strlen(json_encode($ret, JSON_UNESCAPED_UNICODE)),
         ]);
